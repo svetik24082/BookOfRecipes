ca0d46fa587a664f8b54972dbd54f032aa276abf
Света <svetik24082@mail.ru>
2022-12-22 16:06:51 +0500

    1 коммит

---
+++ src/main/java/com/example/bookofrecipes/BookOfRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookofrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOfRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookOfRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookofrecipes/BookOfRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookofrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOfRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
